b9b806bb759d4eebed089cb5664a4df83e877091
xavier <vigenesisvi@gmail.com>
2025-02-19 17:33:30 -0700

    added verbose and size arguments to data_generator; modified values published

--- src/px100_controller/px100_controller/data_generator.py
+++ src/px100_controller/px100_controller/data_generator.py
@@ -22,6 +22,7 @@ class DataGenerator(InterbotixManipulatorXS):
             robot_name=pargs.robot_name,
             args=args,
         )
+        self.verbose = pargs.verbose
         self.publisher = self.core.get_node().create_publisher(
             C,
             'position_data',
@@ -71,28 +72,30 @@ class DataGenerator(InterbotixManipulatorXS):
         # if the joint angles are not a valid configuration
         # return
         if not self.arm.set_joint_positions(thetalist):
-            self.log_info(f'Joint angles: {thetalist} failed')
+            if self.verbose:
+                self.log_info(f'Joint angles: {thetalist} failed')
             return
 
-        fk_position = mr.FKinSpace(M, Slist, thetalist)
+        # a transformation matrix representing the pose of the robot arm
+        T_sb = mr.FKinSpace(M, Slist, thetalist)
         rot_matrix = np.eye(4)
-        rot_matrix[:3, :3] = fk_position[:3, :3] # columns 0,1,2 and rows 0,1,2
+        rot_matrix[:3, :3] = T_sb[:3, :3] # columns 0,1,2 and rows 0,1,2
         rpy = ang.rotation_matrix_to_euler_angles(rot_matrix)
-        
-        # computing the virtual frame position
-        virtual_fk_position = np.linalg.inv(rot_matrix) @ fk_position
+
         msg = C()
-        msg.x = virtual_fk_position[0, 3]
-        # msg.position.y = cartesian_position[1]
-        # msg.position.z = cartesian_position[2]
-        msg.y = virtual_fk_position[2, 3] # z-axis is our vertical
+        msg.x = T_sb[0, 3]
+        msg.y = T_sb[1, 3]
+        msg.z = T_sb[2, 3]
         msg.pitch = rpy[1]
-        # msg.yaw = rpy[2]
-        # msg.waist = thetalist[0]
+        msg.yaw = rpy[2]
+        msg.waist = thetalist[0]
         msg.shoulder = thetalist[1]
         msg.elbow = thetalist[2]
         msg.wrist = thetalist[3]
-        self.log_info(f'publishing {thetalist}')
+        
+        if self.verbose:
+            self.log_info(f'publishing {thetalist}')
+
         self.publisher.publish(msg)
 
     def log_info(self, msg):
@@ -103,6 +106,8 @@ def main(args=None):
     p = argparse.ArgumentParser()
     p.add_argument('--robot_model')
     p.add_argument('--robot_name', default=None)
+    p.add_argument('--size', default=1_000)
+    p.add_argument('--verbose', default=False)
     p.add_argument('args', nargs=argparse.REMAINDER)
 
     command_line_args = remove_ros_args(args=sys.argv)[1:]
