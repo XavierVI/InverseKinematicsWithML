e83c1ca18ed0e50e34136cf5f5822b3c5cad6d8f
xavier <vigenesisvi@gmail.com>
2025-05-23 10:51:17 -0600

    added three new packages for the project; removed old package with unused code

--- src/ik_with_scikit_learn/setup.py
+++ src/ik_with_scikit_learn/setup.py
@@ -1,29 +1,25 @@
 from setuptools import find_packages, setup
 
-package_name = 'px100_controller'
+package_name = 'ik_with_scikit_learn'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', 
-            ['launch/data_generator.launch.py', 'launch/px100_controller.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='xavier',
     maintainer_email='vigenesisvi@gmail.com',
     description='TODO: Package description',
-    license='Apache-2.0',
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'pytorch_controller = px100_controller.pytorch_controller:main',
-            'data_generator = px100_controller.data_generator:main'
         ],
     },
 )
